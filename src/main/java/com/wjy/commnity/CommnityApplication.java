537a7673a183f62a8bdcec448ada748055cabee9
wjy520111 <=>
2019-09-01 22:02:10 +0800

    init rap

---
+++ src/main/java/com/wjy/commnity/CommnityApplication.java
@@ -0,0 +1,13 @@
+package com.wjy.commnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  CommnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommnityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjy/commnity/CommnityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wjy.commnity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommnityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
